fc966a6d48261cacf8bf90b6a2ac84df58f9563b
warmb2009 <2462787827@qq.com>
2018-06-25 22:18:30 +0800

    修正h2.py

--- py/h2.py
+++ py/h2.py
@@ -1,32 +1,69 @@
-#!/usr/bin/env python2
+import sys, getopt
 import os
 
-def filecovert(file_path, out_dir):
-	b = bytearray(open(file_path, 'rb').read())
-	
-	isSec = 1
-	
-	if len(b) > 0:
-		if b[0] == 128 :
-			del b[0]
-			isSec = 0
-	for i in range(len(b)):
-		b[i] ^= 0x78
-	newfilename =  out_dir + '/' + file_path
-	
-	if isSec == 1:
-		b.insert(0, 128)
-			
-	open(newfilename, 'wb').write(b)
+
+class h2():
+    
+    def data_convert(from_data):
+        b = bytearray(from_data)
+        decode = True
+        if len(b) > 0:
+            if b[0] == 128:
+                del b[0]
+                decode = False
+        for i in range(len(b)):
+            b[i] ^= 0x78
+        if decode:
+            b.insert(0, 128)
+        b = b.decode('gbk').encode('utf8')
+        return b
+    
+    @classmethod
+    def file_convert(self, from_file, to_file):
+        ff = open(from_file, 'rb')
+        read = ff.read()
+        # coding = detect(read)['encoding']
+        # print(coding)
+        # data = self.data_convert(read.decode(coding).encode('utf8'))
+        data = self.data_convert(read)
+
+        ff.close()
+
+        tf = open(to_file, 'wb')
+        tf.write(data)
+        tf.close()
+    
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print("test.py -i <inputfile> -o <outputfile>")
+        sys.exit(2)
+
+    inputfile = ''
+    outputfile = ''
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print("help: test.py -i <inputfile> -o <outputfile>")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    if outputfile == '':
+        filename = os.path.basename(inputfile).split('.')[0]
+        outputfile = './%s_new.txt' % filename
+    try:
+        f = open(inputfile)
+        f.close()
+        h2.file_convert(inputfile, outputfile)
+    except IOError:
+        print("File is not accessible")
+
+    print('创建文件成功')
+    print(os.path.abspath(outputfile))
 
 if __name__ == '__main__':
-	
-	out_dir = 'out'
-	
-	if not os.path.isdir(out_dir):
-		os.mkdir(out_dir)
-	
-	for i in os.listdir('.'):
-		if i[i.rfind('.'):] == '.txt':
-			filecovert(i, out_dir)
-			print i[:i.find('.')]
+    main(sys.argv[1:])
